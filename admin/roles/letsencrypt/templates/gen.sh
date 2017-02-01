f7815fcff0072252f5874ee48f91c34d59497ef5
Jan Skalny <jan@skalny.sk>
2017-02-01 12:32:55 +0100

    new roles and updated existing backup/monitoring/base roles

--- admin/roles/letsencrypt/templates/gen.sh
+++ admin/roles/letsencrypt/templates/gen.sh
@@ -2,4 +2,4 @@
 
 CMD="/root/ssl/certbot-auto certonly -c cli.ini"
 
-$CMD -d {{ansible_domain}} -d www.{{ansible_domain}} -d {{inventory_hostname}} 
+$CMD -d {{ansible_domain}} -d www.{{ansible_domain}} -d {{inventory_hostname}} -d mail.{{ ansible_domain }}
